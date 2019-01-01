2357446ab8a99a9911e8b9eedff43c84a5b54cc3
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2019-01-01 22:26:56 +0530

    counting sort

--- src/sorting/FraudulentActivityNotifications.java
+++ src/sorting/FraudulentActivityNotifications.java
@@ -1,119 +1,34 @@
 package sorting;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 /**
  * https://www.hackerrank.com/challenges/fraudulent-activity-notifications
  *
  */
 public class FraudulentActivityNotifications {
 
-    /*static int[] insertionSort(int[] inputArr){
-        int temp;
-        int[] arr = inputArr;
-        for(int i = 1; i < arr.length; i++){
-            for(int j = i; j > 0; j--){
-                if(arr[j] < arr[j - 1]){
-                    temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
-            }
-        }
-        return arr;
-    }*/
-
-    static void insertionSort(int[] arr){
-        int temp;
-        for(int i = 1; i < arr.length; i++){
-            for(int j = i; j > 0; j--){
-                if(arr[j] < arr[j - 1]){
-                    temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
-            }
-        }
-    }
-
-    static float median(ArrayList<Integer> arr){
-        int size = arr.size(), half = size / 2;
-        if(size % 2 == 0){
-            if(size > 1){
-                return (arr.get(half) + arr.get(half + 1)) / 2;
-            }
-            return 0;
-        }else{
-            return arr.get(half);
-        }
-    }
-
-    static float median(int[] arr){
-        int size = arr.length, half = size / 2;
-        if(size % 2 == 0){
-            if(size > 1){
-                return (arr[half] + arr[half + 1]) / 2;
-            }
-            return 0;
-        }else{
-            return arr[half];
-        }
-    }
-
-    /*static int activityNotifications(int[] expenditure, int d) {
-        int notifications = 0;
-        if(expenditure.length != 0 && d < expenditure.length){
-            ArrayList<Integer> trailingDays = new ArrayList<>();
-            for(int i = 0; i < expenditure.length; i++){
-                if(i >= d){
-                    Collections.sort(trailingDays);
-                    if(median(trailingDays) * 2 <= expenditure[i]){
-                        notifications++;
-                    }
-                    trailingDays.remove(new Integer(expenditure[i - d]));
-                }
-                trailingDays.add(expenditure[i]);
-            }
-        }
-        return notifications;
-    }*/
-
-    static void insertionHelper(int[] arr, int toRemove, int toInsert){
-        if(toRemove == toInsert){
-            return;
-        }
-        boolean inserted = false;
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] == toRemove){
-                arr[i] = arr[i+1];
-                arr[i+1] = toRemove;
-            }
-            if(arr[i] > toInsert){
-                arr[i+1] = arr[i];
-                arr[i] = toInsert;
-                inserted = true;
-                break;
-            }
-        }
-        if(!inserted){
-            arr[arr.length - 1] = toInsert;
-        }
-    }
-
     static int activityNotifications(int[] expenditure, int d) {
         int notifications = 0;
         if(expenditure.length != 0 && d < expenditure.length){
-            int[] trailingDays = new int[d];
+            int counter[] = new int[201];
             for(int i = 0; i < d; i++){
-                trailingDays[i] = expenditure[i];
+                counter[expenditure[i]]++;
             }
-            insertionSort(trailingDays);
+            int i1 = (int)Math.floor((float)(d - 1) / 2);
+            int i2 = (int)Math.ceil((float)(d - 1) / 2);
+            int m1 = 0, m2 = 0;
             for(int i = d; i < expenditure.length; i++){
-                if(median(trailingDays) * 2 <= expenditure[i]){
+                for(int j = 0, k = 0; k <= i1; k += counter[j], j++){
+                    m1 = j;
+                }
+                for(int j = 0, k = 0; k <= i2; k += counter[j], j++){
+                    m2 = j;
+                }
+                float m = (float)(m1 + m2) / 2;
+                if(m * 2 <= expenditure[i]){
                     notifications++;
                 }
-                insertionHelper(trailingDays, expenditure[i - d], expenditure[i]);
+                counter[expenditure[i - d]]--;
+                counter[expenditure[i]]++;
             }
         }
         return notifications;
@@ -128,8 +43,8 @@ public class FraudulentActivityNotifications {
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 3, 4, 2, 3, 6, 8, 4, 5};
-        System.out.println(activityNotifications(arr, 5));
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println(activityNotifications(arr, 3));
     }
 
 }
