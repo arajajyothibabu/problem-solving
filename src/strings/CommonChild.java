03dad337fbcf04b574c1f8adf3ef238a665bd71b
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2019-01-06 18:36:04 +0530

    lcs with dp understood finally

---
+++ src/strings/CommonChild.java
@@ -0,0 +1,28 @@
+package strings;
+
+public class CommonChild {
+
+    static int commonChild(String s1, String s2) {
+        char[] s1c = s1.toCharArray();
+        char[] s2c = s2.toCharArray();
+        int len = s1c.length;
+        int[][] matrix = new int[len][len];
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len; j++){
+                if(s1c[i] == s2c[j]){
+                    matrix[i][j] = matrix[Math.max(0, i - 1)][Math.max(0, j - 1)] + 1;
+                }else{
+                    matrix[i][j] = Math.max(matrix[Math.max(0, i - 1)][j], matrix[i][Math.max(0, j - 1)]);
+                }
+            }
+        }
+        return matrix[len - 1][len - 1];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "SHINCHAN";
+        String s2 = "NOHARAAA";
+        System.out.println(commonChild(s1, s2));
+    }
+
+}
